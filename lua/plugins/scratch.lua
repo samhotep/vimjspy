a2159a096b9d14c8199fc2c797503c5ea0a9fe2e
Samuel Olwe <sam.olwe@gmail.com>
2025-05-30 10:17:47 +0300

    Updated scratch

---
+++ lua/plugins/scratch.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = { enabled = true },
+    scratch = {
+      ft = "markdown",
+      cmd = "Scratch",
+      name = "Scratch",
+    },
+  },
+}
