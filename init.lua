824d55a149d0d43adb6a70ddb3f966e8e2dc88a0
Samuel Olwe <sam.olwe@gmail.com>
2025-06-08 23:40:41 +0300

    Added ruff

--- init.lua
+++ init.lua
@@ -1,2 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("lspconfig").ruff.setup({
+  init_options = {
+    settings = {
+      -- Ruff language server settings go here
+      lineLength = 88,
+      indentSize = 4,
+      indentStyle = "space",
+      quoteStyle = "double",
+      lineEnding = "auto",
+    },
+    args = {
+      "--extend-select",
+      "I", -- Example: select specific error codes
+    },
+  },
+})
